f361bf32e2a95fe05e5c059a110fe3b4c49b6cd5
NorejOn <oreonorej@gmail.com>
2023-09-18 20:05:10 +0300

    SpringFinalPart

--- src/main/java/com/example/pyjachok/controllers/ImageController.java
+++ src/main/java/com/example/pyjachok/controllers/ImageController.java
@@ -1,8 +1,12 @@
 package com.example.pyjachok.controllers;
 
+import com.example.pyjachok.dao.EstablishmentDAO;
+import com.example.pyjachok.models.Establishment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,16 +18,23 @@ import java.nio.file.Paths;
 @RestController
 public class ImageController {
 
-    private String uploadDir = "C:/Users/mykol/IdeaProjects/FinishProject/images/";
+    @Autowired
+    private EstablishmentDAO establishmentDAO;
+    private String uploadDir = "C:/Users/mykol/IdeaProjects/FinishProject/Pyjachok/src/main/resources/static/images";
 
     @PostMapping("/image")
-    public ResponseEntity<String>  uploadImage(@RequestBody MultipartFile file) {
+    public ResponseEntity<String>  uploadImage(@RequestBody MultipartFile file,  @RequestParam("name") String name) {
+        System.out.println(name);
         try {
             byte[] imageBytes = file.getBytes();
             String fileName = file.getOriginalFilename();
             Path filePath = Paths.get(uploadDir, fileName);
 
             System.out.println("Save image to: " + filePath.toAbsolutePath());
+            System.out.println(filePath);
+            Establishment establishment  = establishmentDAO.getByName(name);
+           establishment.setPhoto(uploadDir+fileName);
+           establishmentDAO.save(establishment);
 
             Files.write(filePath, imageBytes);
 
